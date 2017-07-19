57e7a28a73d43519232da7a4aaa1aec2c951fd31
colin <crrood@gmail.com>
2017-07-18 18:32:09 -0700

    remove unused files

--- test/clojure_webapp/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-webapp.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-webapp.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
