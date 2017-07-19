d912979899c39827d4600f9bf3503a089a063e8d
colin <crrood@gmail.com>
2017-07-18 22:43:08 -0700

    create seeding

--- src/clojure_webapp/models/migration.clj
+++ src/clojure_webapp/models/migration.clj
@@ -5,13 +5,13 @@
 (defn migrated? []
 	(-> (sql/query tally/spec
 		[(str "select count(*) from information_schema.tables "
-			"where table_name='tallies'")])
-		first :count pos?))
+			"where table_name='tallies' or table_name='tally_groups'")])
+		first :count (= 2)))
 
 (defn migrate []
 	(when (not (migrated?))
 		(try
-			(print "Creating database structure...") (flush)
+			(print "Creating database structure...")
 			(sql/db-do-commands tally/spec
 				(sql/create-table-ddl
 					:tallies
@@ -26,3 +26,26 @@
 					[:groupname :varchar]]))
 			(println " done")
 			(catch Exception e (.getNextException e)))))
+
+(defn seed []
+	(when (and 
+	(migrated?) 
+	(-> (sql/query tally/spec ["select count(*) from tallies"]) first :count zero?)
+	(-> (sql/query tally/spec ["select count(*) from tally_groups"]) first :count zero?))
+		(print "Seeding starting database values...")
+		(tally/new-tally-group)
+		(tally/add-tally 1)
+		(tally/add-tally 1)
+		(println "done")))
+
+(defn reset-db []
+	(when (migrated?)
+		(print "Dropping tables from database...")
+		(sql/db-do-commands tally/spec "drop table tallies")
+		(sql/db-do-commands tally/spec "drop table tally_groups")
+		(println "done")))
+
+(defn reseed []
+	(reset-db)
+	(migrate)
+	(seed))
