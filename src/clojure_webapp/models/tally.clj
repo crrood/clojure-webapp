26cb20382f9831a274e9bb77ba8a09787eddc87c
colin <crrood@gmail.com>
2017-07-18 18:11:35 -0700

    basic CRUD in tally model

--- src/clojure_webapp/models/tally.clj
+++ src/clojure_webapp/models/tally.clj
@@ -3,26 +3,38 @@
 
 (def spec (or (System/getenv "DATABASE_URL") "postgresql://localhost:5432/clojure-webapp"))
 
-(defn new-tally-group []
-	)
+(defn new-tally-group
+	([] 
+		(sql/insert! spec :tally_groups 
+			{:groupname "New Tally Group"}))
+	([^String group-name] 
+		(sql/insert! spec :tally_groups 
+			{:groupname group-name})))
 
 (defn get-tally-groups []
-	(into [] (sql/query spec ["select * from tally_groups order by groupid desc"])))
+	(into [] (sql/query spec 
+		["select * from tally_groups order by groupid desc"])))
 
 (defn load-tally-group [group-id]
 	)
 
-(defn rename-tally-group [group-id new-name]
-	)
+(defn rename-tally-group [^Integer group-id ^String new-name]
+	(sql/update! spec :tally_groups 
+		{:groupname new-name} ["groupid = ?" group-id]))
 
-(defn add-tally [group-id]
-	)
+(defn add-tally
+	([group-id] 
+		(sql/insert! spec :tallies 
+			{:groupid group-id :tallycount 0 :tallyname "New Tally"}))
+	([group-id tally-name] 
+		(sql/insert! spec :tallies 
+			{:groupid group-id :tallycount 0 :tallyname tally-name})))
 
 (defn remove-tally [tally-id]
-	)
+	(sql/delete! spec :tallies ["id = ?" tally-id]))
 
 (defn rename-tally [tally-id new-name]
-	)
+	(sql/update! spec :tallies {:tallyname new-name} ["id = ?" tally-id]))
 
 (defn increment-tally [tally-id]
 	)
@@ -31,7 +43,7 @@
 	)
 
 (defn reset-tally [tally-id]
-	)
+	(sql/update! spec :tallies {:tallycount 0} ["id = ?" tally-id]))
 
 (defn reset-all-in-group [group-id]
 	)
